2cf34b5dc8ea11a85431b3740ab9c1ad287f37e6
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2024-09-26 18:45:42 -0300

    explicar como ajuda o useEffect neste caso Authenticator

--- login-cognito-golang-microservice/src/CognitoLogin/CognitoLogin.tsx
+++ login-cognito-golang-microservice/src/CognitoLogin/CognitoLogin.tsx
@@ -4,71 +4,61 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 import awsconfig from '../../aws-exports';
 
-import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
 Amplify.configure(awsconfig);
 
 async function currentSession() {
-    try {
-        const { tokens } = await fetchAuthSession({ forceRefresh: true });
+  try {
+    const { tokens } = await fetchAuthSession({ forceRefresh: true });
 
-        if (tokens!) {
-            console.log(tokens?.accessToken.toString());
-        } else {
-            console.log("TOKEN is NULL");
-        }
-    } catch (err) {
-        console.log(err);
+    if (tokens!) {
+      console.log(tokens?.accessToken.toString());
+    } else {
+      console.log("TOKEN is NULL");
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const CognitoLogin = () => {
+  const [currentUser, setCurrentUser] = useState(""); 
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
 
-    var CurrentUser="";
+  useEffect(() => {
+    console.log("useEffect CurrentUser: " + currentUser + " isLoggingOut: " + isLoggingOut);
+  }, [currentUser, isLoggingOut]); // Certifique-se de incluir 'isLoggingOut' aqui
 
-    const [isLoggingOut, setIsLoggingOut] = useState(false);
+  return (
+    <Authenticator>
+      {({ signOut, user }) => {
+        if (user) {
+          setCurrentUser(user.username); 
+          //console.log("Usuário logado: " + user.username + " set IsLoggingOut: " + isLoggingOut + " CurrentUser: " + currentUser);
+        }
 
-    useEffect(() => {
-        //currentSession();
-        console.log("useEffect "+"  CurrentUser:  "+CurrentUser+ "  isLoggingOut: "+isLoggingOut);
-    }, [CurrentUser]); 
+        const handleSignOut = () => {
+          setIsLoggingOut(true);
+          setCurrentUser(""); 
+          if (typeof signOut === 'function') {
+            signOut();
+          } else {
+            console.error("signOut não está definido");
+          }
+        };
 
-
-    return (
-        <Authenticator>
-            {({ signOut, user }) => {
-                // Atualiza o estado 'currentUser' sempre que o 'user' mudar
-
-                if (user && !isLoggingOut) {
-                    setIsLoggingOut(false);
-                    CurrentUser=user.username;
-                    console.log("Usuário logado: " + user.username+"  set IsLoggingOut: "+isLoggingOut+"  CurrentUser:  "+CurrentUser);
-                } else if (isLoggingOut) {
-                    console.log("Usuário deslogado: "+"  CurrentUser:  "+CurrentUser);
-                    console.log("Usuário deslogado");
-                }
-
-                const handleSignOut = () => {
-                    setIsLoggingOut(true);
-                    // Certifica-se de que 'signOut' está definido antes de chamar
-                    if (typeof signOut === 'function') {
-                        signOut();
-                    } else {
-                        console.error("signOut não está definido");
-                    }
-                }
-
-                return user ? (
-                    <div>
-                        <h1>Olá, {user.username}</h1>
-                        <button onClick={handleSignOut}>Sair</button>
-                    </div>
-                ) : (
-                    <div>Faça login para continuar</div>
-                );
-            }}
-        </Authenticator>
-    );
+        return user ? (
+          <div>
+            <h1>Olá, {user.username}</h1>
+            <button onClick={handleSignOut}>Sair</button>
+          </div>
+        ) : (
+          <div>Faça login para continuar</div>
+        );
+      }}
+    </Authenticator>
+  );
 };
 
 export default CognitoLogin;
